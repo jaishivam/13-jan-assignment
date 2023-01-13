a80201b538701760e598a81f4cbe8ed12b7d3330
jaishivam <94339528+jaishivam@users.noreply.github.com>
2023-01-13 16:49:06 +0530

    Update problem1.cpp

--- problem1.cpp
+++ problem1.cpp
@@ -1,48 +1,41 @@
-#include <iostream>
-#include <string>
-#include <vector>
-
-class hello
+#include<iostream>
+using namespace std;
+class Rectangle
 {
-public:
+private:
 int length;
 int breadth;
-hello()
-{
-int length = 0;
-int breadth = 0;
-}
-hello(int lenght)
-{
-// this(5,8);
-this->length = 4;
-breadth = 5;
-}
-hello(int length, int breadth)
-{
-this->length = 7;
-this->breadth = 8;
-}
-void m1()
-{
-std::cout << length * breadth << std::endl;
-}
-};
-class Recatangle8
-{
 public:
-static void main(std::vector<std::string> &args)
+Rectangle()
 {
-hello* obj = new hello();
-obj->m1();
-hello* obj1 = new hello(10);
-obj1->m1();
-hello* obj2 = new hello(12, 2);
-obj2->m1();
+length=0;
+breadth=0;
 }
+Rectangle(int x)
+{
+length=x;
+breadth=x;
+}
+Rectangle(int x,int y)
+{
+length=x;
+breadth=y;
+}
+int getarea()
+{
+return length*breadth;
+
+}
+
 };
-int main(int argc, char **argv){
-std::vector<std::string> parameter(argv + 1, argv + argc);
-Recatangle8::main(parameter);
-return 0;
-};
+int main()
+{
+Rectangle R1;
+Rectangle R2(10);
+Rectangle R3(2,3);
+
+cout<<R1.getarea()<<endl;
+cout<<R2.getarea()<<endl;
+cout<<R3.getarea()<<endl;
+
+}
